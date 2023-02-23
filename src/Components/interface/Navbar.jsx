be9b1e725ede7c46755d948b24d1e9fb0d139a36
Jefferson Lopez <Jeffersonlopezmendoza343@gmail.com>
2023-02-23 18:44:17 -0500

    Deploy

--- src/Components/interface/Navbar.jsx
+++ src/Components/interface/Navbar.jsx
@@ -1,17 +1,15 @@
 import React, { useState } from "react";
 
-import { Defect } from "./Defect";
-import { Home } from "./Seccions/Home";
 import { Lerning } from "./Seccions/Lerning";
-import { Contact } from "./Seccions/Contact";
-import { Accout } from "./Seccions/Accout";
 import { Chat } from "./Seccions/Chat";
+import { GoNews } from "./Seccions/GoNews";
+import { Login } from "./Seccions/Login";
 
 import logo from "../../assets/logo.svg";
 
 export function Navbar() {
   const [hasClass, setHasClass] = useState(false);
-  const [title, setTitle] = useState("GOARBIT");
+  const [title, setTitle] = useState("GONEWS");
 
   function toggleClass() {
     setHasClass(!hasClass);
@@ -22,49 +20,70 @@ export function Navbar() {
     setHasClass(!hasClass);
   }
 
-  function handleTutorialsClick() {
-    setTitle("TUTORIALS");
-  }
-
   function Go(T) {
     setTitle(T);
     setHasClass();
   }
 
   return (
-    <div className="container-nav">
-      <div className="nav">
-        <div className="navigation">
-          <div className="titleLogo">
-            <img onClick={() => Go("GOARBIT")} src={logo} />
-            <h1>{title}</h1>
-          </div>
-          <div onClick={toggleClass} className="menu">
-            <div className={`line ${hasClass ? "line-clic" : ""}`}></div>
-            <div className={`line ${hasClass ? "line-clic" : ""}`}></div>
-            <div className={`line ${hasClass ? "line-clic" : ""}`}></div>
+    <>
+      <div className="container-nav">
+        <div className="nav">
+          <div className="navigation">
+            <div className="titleLogo">
+              <img onClick={() => Go("GONEWS")} src={logo} />
+              <h1>{title}</h1>
+            </div>
+            <div onClick={toggleClass} className="menu">
+              <div className={`line ${hasClass ? "line-clic" : ""}`}></div>
+              <div className={`line ${hasClass ? "line-clic" : ""}`}></div>
+              <div className={`line ${hasClass ? "line-clic" : ""}`}></div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={`submenu ${hasClass ? "submenu-clic" : ""}`}>
-        <h2 onClick={() => ClicTitle("INICIO")}>INICIO</h2>
-        <h2 onClick={() => ClicTitle("TUTORIALS")}>TUTORIALS</h2>
-        <h2 onClick={() => ClicTitle("CONTACTO")}>CONTACTO</h2>
-        <h2 onClick={() => ClicTitle("CREAR CUENTA")}>CREAR CUENTA</h2>
-        <h2 onClick={() => ClicTitle("CHAT")}>CHAT</h2>
-      </div>
-      {!hasClass && (
-        <div className="contat-seccions">
-          {title === "GOARBIT" && <Defect />}
-          {title === "INICIO" && (
-            <Home onTutorialsClick={handleTutorialsClick} />
-          )}
-          {title === "TUTORIALS" && <Lerning />}
-          {title === "CONTACTO" && <Contact />}
-          {title === "CREAR CUENTA" && <Accout />}
-          {title === "CHAT" && <Chat />}
+        <div className={`submenu ${hasClass ? "submenu-clic" : ""}`}>
+          <h2 onClick={() => ClicTitle("GONEWS")}>GONEWS</h2>
+          <h2 onClick={() => ClicTitle("TUTORIALS")}>TUTORIALS</h2>
+          <h2 onClick={() => ClicTitle("CHAT")}>CHAT</h2>
+          <h2 onClick={() => ClicTitle("CREAR CUENTA")}>CREAR CUENTA</h2>
         </div>
-      )}
-    </div>
+        {!hasClass && (
+          <div className="contat-seccions">
+            {title === "GONEWS" && <GoNews />}
+            {title === "TUTORIALS" && <Lerning />}
+            {title === "CREAR CUENTA" && <Login />}
+            {title === "CHAT" && <Chat />}
+          </div>
+        )}
+      </div>
+
+      <div className="container-nav1">
+        <div className="nav">
+          <div className="navigation">
+            <div className="titleLogo">
+              <img onClick={() => Go("GONEWS")} src={logo} />
+              <h1>{title}</h1>
+            </div>
+            <div className="menu">
+              <div className="d" >
+                <h2 onClick={() => Go("GONEWS")}>GONEWS</h2>
+                <h2 onClick={() => Go("TUTORIALS")}>TUTORIALS</h2>
+                <h2 onClick={() => Go("CHAT")}>CHAT</h2>
+                <h2 onClick={() => Go("CREAR CUENTA")}>CUENTA</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {!hasClass && (
+          <div className="contat-seccions">
+            {title === "GONEWS" && <GoNews />}
+            {title === "TUTORIALS" && <Lerning />}
+            {title === "CREAR CUENTA" && <Login />}
+            {title === "CHAT" && <Chat />}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
---
+++ src/Components/interface/Seccions/GoNews.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function GoNews() {
+  return (
+    <div>
+      <h1>GoNews</h1>
+    </div>
+  );
+}

e5bf90d0d35158485647536a76bcbe4dc53f44fd
Jefferson Lopez <Jeffersonlopezmendoza343@gmail.com>
2023-02-27 10:56:00 -0500

    Deploy

--- src/Components/interface/Nav/Movil/NavMovil.jsx
+++ src/Components/interface/Nav/Movil/NavMovil.jsx
@@ -37,28 +37,48 @@ export function NavMovil() {
   }
 
   return (
-    <div className="container-nav">
-      <div className="nav">
-        <div className="navigation">
-          <div className="titleLogo">
-            <img onClick={() => Go("GONEWS")} src={logo} />
-            <h1>{title}</h1>
-          </div>
-          <div onClick={toggleClass} className="menu">
-            <div className={`line ${hasClass ? "line-clic" : ""}`}></div>
-            <div className={`line ${hasClass ? "line-clic" : ""}`}>
-              <div></div>
-              <div></div>
+    <>
+      <div className="container-nav">
+        <div className="nav">
+          <div className="navigation">
+            <div className="titleLogo">
+              <img onClick={() => Go("GONEWS")} src={logo} />
+              <h1>{title}</h1>
             </div>
-            <div className={`line ${hasClass ? "line-clic" : ""}`}></div>
+            <div onClick={toggleClass} className="menu">
+              <div className={`line ${hasClass ? "line-clic" : ""}`}></div>
+              <div className={`line ${hasClass ? "line-clic" : ""}`}>
+                <div></div>
+                <div></div>
+              </div>
+              <div className={`line ${hasClass ? "line-clic" : ""}`}></div>
+            </div>
           </div>
         </div>
+        <div className={`submenu ${hasClass ? "submenu-clic" : ""}`}>
+          <h2 onClick={() => ClicTitle("GONEWS")}>GONEWS</h2>
+          <h2 onClick={() => ClicTitle("TUTORIALS")}>TUTORIALS</h2>
+          <h2 onClick={() => ClicTitle("CHAT")}>CHAT</h2>
+          <h2 onClick={() => ClicTitle("CREAR CUENTA")}>CUENTA</h2>
+        </div>
       </div>
-      <div className={`submenu ${hasClass ? "submenu-clic" : ""}`}>
-        <h2 onClick={() => ClicTitle("GONEWS")}>GONEWS</h2>
-        <h2 onClick={() => ClicTitle("TUTORIALS")}>TUTORIALS</h2>
-        <h2 onClick={() => ClicTitle("CHAT")}>CHAT</h2>
-        <h2 onClick={() => ClicTitle("CREAR CUENTA")}>CUENTA</h2>
+      <div className="container-nav1">
+        <div className="nav">
+          <div className="navigation">
+            <div className="titleLogo">
+              <img onClick={() => Go("GONEWS")} src={logo} />
+              <h1>{title}</h1>
+            </div>
+            <div className="menu">
+              <div className="d">
+                <h2 onClick={() => Go("GONEWS")}>GONEWS</h2>
+                <h2 onClick={() => Go("TUTORIALS")}>TUTORIALS</h2>
+                <h2 onClick={() => Go("CHAT")}>CHAT</h2>
+                <h2 onClick={() => Go("CREAR CUENTA")}>CUENTA</h2>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       {!hasClass && (
         <div className="contat-seccions">
@@ -75,6 +95,6 @@ export function NavMovil() {
           ) : null}
         </div>
       )}
-    </div>
+    </>
   );
 }
